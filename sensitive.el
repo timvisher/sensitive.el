46545888e0ac54c14426c2c0b7bc6cbada3445e9
Tim Visher <tvisher@monetate.com>
2013-09-08 15:04:09 -0400

    Correcting headers. EOM

--- sensitive.el
+++ sensitive.el
@@ -1,6 +1,19 @@
 ;;; sensitive.el --- A dead simple way to load sensitive information
 
-;; This file is not part of GNU Emacs
+;; CC BY Tim Visher
+
+;; Author: Tim Visher <tim.visher@gmail.com>
+;; Keywords: convenience
+;; Package-Requires: ((emacs "24") (sequences "0.1.0))
+;; Version: 1.0.0
+
+;; This file is not part of GNU Emacs.
+
+;; This work is licensed under the Creative Commons Attribution 3.0
+;; Unported License. To view a copy of this license, visit
+;; <http://creativecommons.org/licenses/by/3.0/> or send a letter to
+;; Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+;; California, 94041, USA.
 
 ;;; Commentary:
 
@@ -10,14 +23,6 @@
 ;; users and the more facilities people have for hiding the
 ;; information they need to load without having to jump through hoops
 ;; to load it should make that easier.
-
-;;; License:
-
-;; [CC BY](http://creativecommons.org/licenses/by/3.0/)
-
-;;; Contributors
-
-;; Tim Visher ([@timvisher](https://twitter.com/timvisher))
 
 ;;; Code:
 
